6f7267dd1785da0d8e5bd273e5dd0667c98fa563
br3w0r Concepts <br3w0r-concepts@outlook.com>
2021-07-04 21:27:30 +0300

    Fix multiple api calls when going back to All Games page

--- gamelist/src/store/modules/gamelist.js
+++ gamelist/src/store/modules/gamelist.js
@@ -2,21 +2,28 @@ import * as api from '@/api/gamelist'
 
 export const gamelist = {
     state: {
+      first: true,
       games: []
     },
     mutations: {
       addGames(state, games) {
         state.games.push(...games);
+      },
+      setFirst(state) {
+        state.first = false
       }
     },
     actions: {
-      async getAllGames({ commit }) {
-        var response = await api.GetAllGames();
-        
-        if (!response.ok) {
-          console.log("!!! Bad status: " + response.status);
-        } else {
-          commit("addGames", response.body)
+      async getAllGames({ commit, state }) {
+        if (state.first) {
+          var response = await api.GetAllGames();
+          
+          if (!response.ok) {
+            console.log("!!! Bad status: " + response.status);
+          } else {
+            commit("addGames", response.body)
+            commit("setFirst")
+          }
         }
       }
     }
