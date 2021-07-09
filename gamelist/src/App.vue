aefaf5ca6eae7cda47247888a7cbde041447e629
br3w0r <br3w0r-concepts@outlook.com>
2021-07-09 14:51:24 +0300

    List menu for profile button

--- gamelist/src/App.vue
+++ gamelist/src/App.vue
@@ -9,51 +9,65 @@
       <v-container>
         <v-row align="center" no-gutters style="height: 50px" class="flex-nowrap">
           <v-col cols="auto">
-          <v-icon>mdi-gamepad-round-up</v-icon>
-          <span id="site-name" class="font-weight-bold ml-2 mr-3">MyGameList</span>
+            <v-icon>mdi-gamepad-round-up</v-icon>
+            <span id="site-name" class="font-weight-bold ml-2 mr-3">MyGameList</span>
 
-          <v-btn to="/" class="font-weight-light" text>
-            <v-icon>mdi-controller-classic-outline</v-icon>
-            <span style="margin-left: .5em">ALL GAMES</span>
-          </v-btn>
-          <v-btn class="font-weight-light" style="margin-right: 1em" text>
-            <v-icon>mdi-format-list-bulleted</v-icon>
-            <span style="margin-left: .5em">MY LIST</span>
-          </v-btn>
+            <v-btn to="/" class="font-weight-light" text>
+              <v-icon>mdi-controller-classic-outline</v-icon>
+              <span style="margin-left: .5em">ALL GAMES</span>
+            </v-btn>
+            <v-btn class="font-weight-light" style="margin-right: 1em" text>
+              <v-icon>mdi-format-list-bulleted</v-icon>
+              <span style="margin-left: .5em">MY LIST</span>
+            </v-btn>
           </v-col>
 
           <v-col>
-          <v-autocomplete
-            v-model="searchValue"
-            :items="items"
-            filled
-            hide-details
-            label="SEARCH"
-            id="search-bar"
-            class="search-bar"
-            append-icon="mdi-magnify"
-          ></v-autocomplete>
+            <v-autocomplete
+              v-model="searchValue"
+              :items="items"
+              filled
+              hide-details
+              label="SEARCH"
+              id="search-bar"
+              class="search-bar"
+              append-icon="mdi-magnify"
+            ></v-autocomplete>
           </v-col>
 
           <v-col sm="1" md="1" lg="2">
-          <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
           </v-col>
 
           <v-col cols="auto">
-          <v-btn
-            to="/about"
-            text
-          >
-            <span>About</span>
-          </v-btn>
+            <v-btn
+              to="/about"
+              text
+              style="margin-right: 1em"
+            >
+              <span>About</span>
+            </v-btn>
           </v-col>
           <v-col cols="auto">
-          <v-btn
-            target="_blank"
-            text
-          >
-            <v-icon>mdi-account-circle-outline</v-icon>
-          </v-btn>
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  text fab small
+                  v-bind="attrs" v-on="on"
+                >
+                  <v-icon>mdi-account-circle-outline</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="item, i in profileItems"
+                  :key="i"
+                  link
+                >
+                  <v-list-item-title v-text="item"></v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </v-col>
         </v-row>
       </v-container>
@@ -69,7 +83,11 @@ export default {
 
   data: () => ({
     items: ["Super Mario Galaxy", "Doom", "Age of Empires II", "Grand Theft Auto V", "Minecraft"],
-    searchValue: ""
+    searchValue: "",
+    profileItems: [
+      "My Profile",
+      "Sign Out"
+    ]
   }),
 };
 </script>
