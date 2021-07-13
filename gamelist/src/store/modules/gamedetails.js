13b9a25ddcd7a759d162a6b732a3475000473223
br3w0r Concepts <br3w0r-concepts@outlook.com>
2021-07-13 20:47:39 +0300

    Removed test values, getGameDetails return response status when fails

--- gamelist/src/store/modules/gamedetails.js
+++ gamelist/src/store/modules/gamedetails.js
@@ -4,38 +4,9 @@ export const gamedetails = {
     namespaced: true,
     state: {
         show: false,
-        game: {
-            "id": 1,
-            "name": "The Legend of Zelda: Ocarina of Time",
-            "image_url": "https://static.metacritic.com/images/products/games/2/70c7a7d5ef1a36a69018111cc1478f30-98.jpg",
-            "year_released": 1998,
-            "user_list": 0
-        },
-        platforms: [
-            {
-                "name": "Nintendo 64"
-            },
-            {
-                "name": "Dickgame 1000"
-            },
-            {
-                "name": "Dickgame 1000"
-            },
-            {
-                "name": "Dickgame 1000"
-            },
-            {
-                "name": "Dickgame 1000"
-            }
-        ],
-        genres: [
-            {
-                "name": "Action Adventure"
-            },
-            {
-                "name": "Fantasy"
-            },
-        ]
+        game: {},
+        platforms: [],
+        genres: []
     },
     mutations: {
         addDetails(state, {game, platforms, genres}) {
@@ -49,7 +20,7 @@ export const gamedetails = {
             let response = await api.GetGameDetails(gameId);
             if (!response.ok) {
                 console.log("failed to get game details: " + response.status + " : " + JSON.stringify(response.body));
-                return false
+                return response.status
             }
             commit("addDetails", response.body);
             return true
